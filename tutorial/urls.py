51ba3769337e6809df6e3c0152377da6f7f5a77d
Chris DiMaio <chrisdimaio@gmail.com>
2018-04-18 22:04:50 -0400

    Making room for cdimaio's subproject

--- tutorial/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^word_display/', include('word_display.urls')),
-]
--- word_display/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from .models import word
-from .models import users_def
-
-admin.site.register(word)
-admin.site.register(users_def)
